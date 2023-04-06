ab2afcccf4ba94274862ebab586995fbfe6bd3ba
Ly Nguyen <104514970+lynguyen-mia@users.noreply.github.com>
2023-04-06 18:47:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check winner
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
 
